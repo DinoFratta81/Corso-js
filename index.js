9df573fcfa30d5b91f3abbdedbb773ad40e93108
DinoFratta81 <dinofratta2008@gmail.com>
2026-02-27 19:34:48 +0100

    First javascript lessons: variables and constants

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log('Prima Prova JS')
+alert('Iniziamo!')
+
+// let firstNumber = 50;
+// let secondNumber = 20;
+// let operation = "add"
+
+// console.log("CALCOLATRICE SEMPLICE")
+// console.log(`Primo numero: ${firstNumber}`)
+// console.log(`Secondo Numero: ${secondNumber}`)
+// console.log(`operazione ${operation}`)
+
+// let result;
+// if (operation === "add") {
+//     result= firstNumber+ secondNumber;
+//     console.log(`Risultato: ${firstNumber} + ${secondNumber} = ${result}`)
+//     } else {
+//         console.log("Operazione non supportata")
+//     }
+
+let nome = "Dino"; // inizializzazione
+
+nome = "Giacomo"; //riassegnazione
+
+
+let nomeCompleto //dichiarazione (non ci possono essere due variabili con lo stesso nome, come falco e falco)
+
+nomeCompleto = "Antonio"; //assegnazione
+
+nomeCompleto = "Massimo"; //riassegnazione
+
+let n_diamanti = "0"
+
+n_diamanti = "10"
+
+const LIVELLO_IDEALE_PER_DIAMANTI = -18
+
+console.log(nomeCompleto)
+console.log(`Il livello di spawn del diamente è: ${LIVELLO_IDEALE_PER_DIAMANTI}`)
+
+
+
+
+
+
+
+
