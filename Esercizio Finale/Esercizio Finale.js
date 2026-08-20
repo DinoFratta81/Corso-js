61f678188a9880a7110d27172c53d419b0b90cf9
DinoFratta81 <dinofratta2008@gmail.com>
2026-08-20 20:40:15 +0200

    Did some changes

--- Esercizio Finale/Esercizio Finale.js
+++ Esercizio Finale/Esercizio Finale.js
@@ -5,22 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
     loadPokemon(0);
 });
 
-
 async function loadPokemon(offset = 0) {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=40&offset=${offset}`);
         if (!response.ok) {
-            throw new Error("Errore nel caricamento");
+            throw new Error("Errore rilevato")
         }
 
         const data = await response.json();
         currentOffset = offset;
         console.log(data);
 
-
-
         const pokemonPromises = data.results.map(async (pokemon, index) => {
-            const detailResponse = await fetch(pokemon.url)
+            const detailResponse = await fetch(pokemon.url);
             const detail = await detailResponse.json();
 
             return {
@@ -33,40 +30,40 @@ async function loadPokemon(offset = 0) {
 
         currentPokemonList = await Promise.all(pokemonPromises);
 
+
         displayPokemonTable(currentPokemonList);
         updatePagination(data, offset);
 
-
         console.log(currentPokemonList);
+
     } catch (error) {
         console.error(error);
     }
 };
 
 
-
-
 function displayPokemonTable(pokemonList) {
     const tbody = document.getElementById("pokemonTableBody");
 
-    tbody.innerHTML = ``
+    tbody.innerHTML = ``;
+
 
     pokemonList.forEach((pokemon, index) => {
-        const row = document.createElement(`tr`);
-        const tipiHTML = pokemon.types.map(t => `<span class = "${t.type.name}">${t.type.name}</span>`)
+        const row = document.createElement("tr");
+        const tipiHTML = pokemon.types.map(t => `<span class =" ${t.type.name}">${t.type.name}</span>`);
 
         row.innerHTML = `
         <td>
-                  <img src = "${pokemon.sprites.front_default}"
-                  alt = "${pokemon.name}"
-                  class = "pokemon-image">
-
+               <img src = " ${pokemon.sprites.front_default}"
+               alt = ${pokemon.name}
+               class = ""pokemon-image>
         </td>
-             <td class = "pokemon-name">${pokemon.name}</td>       
-             <td class = "pokemon-id">${pokemon.id.toString().padStart(3, `0`)}</td>       
-             <td class = "pokemon-types">${tipiHTML.join(" ")}</td>       
-             <td>${(pokemon.height / 10).toFixed(1)}m</td>         
-             <td>${(pokemon.weight / 10).toFixed(1)}kg</td>        
+
+             <td class = "pokemon-name">${pokemon.name}</td>
+             <td class = "pokemon-id">${pokemon.id.toString().padStart(3, `0`)}</td>
+             <td class = "pokemon-type">${tipiHTML.join(" ")}</td>
+             <td class = "pokemon-name">${(pokemon.height / 10).toFixed(1)}m</td>
+             <td class = "pokemon-name">${(pokemon.weight / 10).toFixed(1)}kg</td>
         `
 
 
@@ -75,9 +72,9 @@ function displayPokemonTable(pokemonList) {
 };
 
 function updatePagination(data, offeset) {
-    const pageInfo = document.getElementById("pageInfo");
-    const currentPage = Math.floor(offeset / 20) + 1;
-    const totalPages = Math.ceil(data.count / 20);
+    let pageInfo = document.getElementById("pageInfo");
+    let currentPage = Math.floor(offeset / 40) + 1;
+    let totalPages = Math.ceil(data.count / 40);
 
     pageInfo.textContent = `Pagina ${currentPage} di ${totalPages}. Numero totale pokemon = ${data.count}.`
 }
